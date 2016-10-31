Added precision, recall and fixed payoff calculation

classifier.py
@@ -2,24 +2,32 @@ from __future__ import division
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import KFold
 # from sklearn.model_selection import KFold, train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 import pandas as pd
 import numpy as np
 
 def cross_validate(X, y):
     kf = KFold(len(X), n_folds=10, shuffle=True)
     print X.columns
-    scores = []
+    accuracies = []
     conf = []
+    precisions = []
+    recalls = []
     for train, test in kf:
         X_train, X_test, y_train, y_test = X.as_matrix()[train], X.as_matrix()[test], y.as_matrix()[train], y.as_matrix()[test]
         clf = LogisticRegression()
         clf.fit(X_train, y_train)
-        conf.append(confusion_matrix(y_test, clf.predict(X_test)))
-        scores.append(clf.score(X_test, y_test))
-    print np.average(scores)
+        predictions = clf.predict(X_test)
+        conf.append(confusion_matrix(y_test, predictions))
+        recalls.append(recall_score(y_test, predictions))
+        precisions.append(precision_score(y_test, predictions))
+        accuracies.append(clf.score(X_test, y_test))
+    print 'Accuracy: ' + str(np.average(accuracies))
+    print 'Precision: ' + str(np.average(precisions))
+    print 'Recall: ' + str(np.average(recalls))
     confusion = np.zeros((2,2))
-    for i in range(len(conf)): confusion += conf[i]
+    for i in range(len(conf)):
+        confusion += conf[i]
     print confusion
 
 